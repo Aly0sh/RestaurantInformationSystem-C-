05c176592b11e33c2d79f1b19aba3168e8a072c6
Алакен Кубатбеков <kubatbekovalaken@gmail.com>
2023-04-14 01:40:07 +0600

    try to connect oracle db

--- RestaurantInformationSystem/Form1.cs
+++ RestaurantInformationSystem/Form1.cs
@@ -1,8 +1,9 @@
+using Oracle.ManagedDataAccess.Client;
+
 namespace RestaurantInformationSystem
 {
     public partial class Form1 : Form
     {
-       
 
         public Form1()
         {
@@ -12,6 +13,13 @@ namespace RestaurantInformationSystem
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OracleConnection con = new OracleConnection(@"Uid=sys;password=t110902a123456789");
+
+            con.Open();
         }
     }
 }
